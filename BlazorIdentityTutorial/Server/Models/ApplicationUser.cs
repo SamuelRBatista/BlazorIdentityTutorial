a190d2d93b975893e137ce5b9697a145280f2455
SamuelRBatista <samuel.r.batista@outlook.com>
2024-11-08 09:34:07 -0300

    first commit

---
+++ BlazorIdentityTutorial/Server/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using BlazorIdentityTutorial.Shared;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorIdentityTutorial.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public List<SuperHero> SuperHeroes { get; set; } = new List<SuperHero>();
+
+    }
+}
